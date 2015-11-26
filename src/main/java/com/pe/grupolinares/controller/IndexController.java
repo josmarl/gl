44e0772a4cf4be36a33051253916109783dce358
unknown <josmarl@github.com>
2015-11-25 22:24:54 -0500

    gl

---
+++ src/main/java/com/pe/grupolinares/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.pe.grupolinares.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("index")
+public class IndexController {
+
+	@RequestMapping("/hello")
+	public ModelAndView index(){
+		return new ModelAndView("index");
+	}
+}
